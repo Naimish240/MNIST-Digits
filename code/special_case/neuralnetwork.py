a05991d2f3f6021fc8de5e829fe21908ed867f3e
Naimish <naimish240@gmail.com>
2019-03-16 11:58:18 +0530

    updates

--- code/special_case/neuralnetwork.py
+++ code/special_case/neuralnetwork.py
@@ -1,20 +1,23 @@
 # Program to create a neural network with input layer,
 # output layer, and one hidden layer.
 
+# import statements
 from random import random
+from math import exp
 
 # Initialize the network
 def init_network(inputs, hidden, outputs):
-    # -------------------------------------------
+    # ----------------------------------------------------
     # INPUT:
-    # -------------------------------------------
-    # inputs  : number of inputs
-    # hidden  : number of neurons in hidden layer
-    # outputs : length of output layer
+    # ----------------------------------------------------
+    # inputs  : int : number of inputs
+    # hidden  : int : number of neurons in hidden layer
+    # outputs : int : length of output layer
+    # ----------------------------------------------------
     # OUTPUT: 
-    # -------------------------------------------
-    # network : array of layers in the network
-    # -------------------------------------------
+    # ----------------------------------------------------
+    # network : array : layers in the network, sans input
+    # ----------------------------------------------------
     
     network = []
     hidden_layer = {}
@@ -38,7 +41,80 @@ def init_network(inputs, hidden, outputs):
     
     # returns network
     return network
+
+# Function to calculate dot product
+def dot(weights, bias, inputs):
+    # ----------------------------------------------------
+    # INPUT:
+    # ----------------------------------------------------
+    # weights : list  : contains weights from network
+    # bias    : float : contains bias value from network
+    # inputs  : list  : contains the input matrix
+    # ----------------------------------------------------
+    # OUTPUT: 
+    # ----------------------------------------------------
+    # sum : float : value of dot product + bias
+    # ----------------------------------------------------
     
+    sum = 0
+    
+    # Calculates dot product
+    for i in range(len(weights)):
+        sum += weights[i] * input[i]
+    
+    # Adds bias
+    sum += bias
+    
+    # Returns the value
+    return sum
+    
+# Function to calculate activation of neurons
+def sigmoid(val):
+    # ----------------------------------------------------
+    # INPUT:
+    # ----------------------------------------------------
+    # val : float : value to calculate sigmoid for
+    # ----------------------------------------------------
+    # OUTPUT: 
+    # ----------------------------------------------------
+    # x : float : sigmoid of input
+    # ----------------------------------------------------
+    
+    x = 1.0 / (1 + exp(-val))
+    return x
+    
+# Function to forward propagate through the network
+def forward_propagation(network, inputs):
+    # ----------------------------------------------------
+    # INPUT:
+    # ----------------------------------------------------
+    # network : list : the matrix which contains the network
+    # inputs  : list : the inputs for the network 
+    # ----------------------------------------------------
+    # OUTPUT: 
+    # ----------------------------------------------------
+    # output : list : returns the output layer
+    # ----------------------------------------------------
+
+    # loop to perform forward propagation
+    for layer in network:
+        # Stores output for layer
+        output = []
+        # Loop to calcualte outputs
+        for neuron in layer:
+            # Calcuating dot product
+            dot_product = dot(neuron['weights'], neuron['bias'], inputs)
+            # Applying sigmoid to output
+            neuron['output'] = sigmoid(dot_product)
+            # Adding the value to the output
+            output.append(neuron['output'])
+            
+        # Updating output for next iteration
+        inputs = output
+    
+    # Returning the output matrix
+    return inputs
+            
 if __name__ == '__main__':
     network = init_network(5, 9, 2)
     
